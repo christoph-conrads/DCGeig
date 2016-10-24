774d1ecaf050325f998029a04bfa086f8327706e
Christoph Conrads <gitlab@christoph-conrads.name>
2016-10-24 14:36:13 +0200

    Experiments: print stats polynomial iteration
    Print the magnification ratio for certain eigenvalues.

--- experiment/plot_inverse_iteration_polynomials.py
+++ experiment/plot_inverse_iteration_polynomials.py
@@ -85,8 +85,42 @@ def fit_chebyshev_jackson_polynomial(k, f):
 
 
 
+def make_heaviside_approximation(k, a, b):
+    assert isinstance(k, int)
+    assert k >= 0
+
+    is_odd = lambda n: (n%2) == 1
+
+    c, e = get_ellipse(a, b)
+    cs = fit_chebyshev_jackson_polynomial(k, heaviside)
+    cs[2::2] = 0
+    if is_odd(k):
+        cs[-1] = 0
+
+    def f(xs):
+        return NPC.chebval((xs-c)/e, cs)
+
+    return f
+
+
+
 def cayley(xs, p):
     return 1/xs * (xs - p)
+
+
+
+def print_stats(label, make_f, degrees, a, b, x2, x1, x0, m=100):
+    assert isinstance(a, numbers.Real)
+    assert isinstance(b, numbers.Real)
+    assert a < b
+
+    fmt = '{:24s} {:2d}  {:8.2e} {:8.2e}  {:8.2e}'
+
+    for k in degrees:
+        f = make_f(k, a, b)
+        g = lambda x: f(1/x) / f(1/x0)
+
+        print fmt.format( label, k, g(x2), g(x1), f(1/x2)/f(1/x1) )
 
 
 
@@ -94,8 +128,7 @@ def main(argv):
     lambda_1 = 0.05;
     lambda_c = 1.0;
     a = 10.0 * lambda_c;
-    b = NP.sqrt(2) * 10.0 * lambda_c;
-    c, e = get_ellipse(1/b, 1/a)
+    b = float('inf')
 
     left = lambda_1
     right = 30*lambda_c
@@ -104,11 +137,25 @@ def main(argv):
     xs = NP.linspace(left, right, num=1000)
     assert NP.all( xs > 0 )
 
-    chebyshev_label='Chebyshev({:d}, {:.1f}, {:.1f})'
+
+    print_stats( \
+        '1/x', lambda k, a, b: (lambda x: x),
+        [1], 0, 1.0/a, lambda_1, lambda_c, a)
+    print_stats( \
+        'Chebyshev', make_chebyshev_polynomial,
+        [2,3,6], 0, 1.0/a, lambda_1, lambda_c, a)
+    print_stats( \
+        'Chebyshev-Jackson', make_heaviside_approximation,
+        [5,7], 0, 1.0/(2*a), lambda_1, lambda_c, a)
+    print_stats( \
+        'Chebyshev-Jackson(shift)', make_heaviside_approximation,
+        [5,7], 0, 1.0/(2*a), lambda_1+lambda_c, lambda_c+lambda_c, a+lambda_c)
 
 
     # plot chebyshev polynomial damping
-    for k in range(2, 4):
+    chebyshev_label='Chebyshev({:d}, {:.1f}, {:.1f})'
+
+    for k in [2,3,6]:
         f = make_chebyshev_polynomial(k, 1/b, 1/a)
         ys = f(1/xs) / f(1/x0)
 
@@ -119,28 +166,16 @@ def main(argv):
     cs = 50 * cayley(xs, a) / cayley(lambda_c, a)
     PP.plot(xs, abs(cs), 'k:', linewidth=2.0, label='Cayley')
 
+
     # 1/x
     PP.plot(xs, x0/xs, 'k', label='1/x')
-
-    # damp interval [1/infty, 1/a]
-    k = 2
-    inf = float('inf')
-    f_inf = make_chebyshev_polynomial(k, 0, 1/a)
-    ys = f_inf(1/xs) / f_inf(1/x0)
-    PP.plot( xs, abs(ys), label=chebyshev_label.format(k, a, inf) )
 
 
     # heaviside + chebyshev-jackson polynomial damping
     fmt = 'Chebyshev-Jackson({:d})'
-    c_h, e_h = get_ellipse(0, 1.0/(2*a))
 
-    for k in [3, 7]:
-        cs = fit_chebyshev_jackson_polynomial(k, heaviside)
-        cs[2::2] = 0
-        degree = k if (k%2 == 0) else k-1
-        cs = cs[:degree+1]
-
-        f = lambda xs: NPC.chebval( (xs-c_h)/e_h, cs )
+    for k in [3, 5, 7]:
+        f = make_heaviside_approximation(k, 0, 1.0/(2*a))
         ys = f(1/xs) / f(1/x0)
 
         PP.plot( xs, abs(ys), '--', label=fmt.format(k) )
