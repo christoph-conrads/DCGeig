e0b52d8b2a58fce9656338ecac11f514bd9eb767
Christoph Conrads <gitlab@christoph-conrads.name>
2016-11-01 19:17:07 +0100

    Src: implement eigenvalue count estimator

---
+++ python/dcgeig/solver.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Copyright 2016 Christoph Conrads
+#
+# This file is part of DCGeig and it is subject to the terms of the DCGeig
+# license. See http://DCGeig.tech/license for a copy of this license.
+
+import numbers
+
+import copy
+
+import numpy as NP
+import numpy.random
+import numpy.linalg as LA
+
+import scipy.sparse as SS
+
+import dcgeig.binary_tree as binary_tree
+import dcgeig.polynomial as polynomial
+import dcgeig.sparse_tools as sparse_tools
+
+
+
+def estimate_trace(f, b, k, eps, P, node):
+    assert callable(f)
+    assert isinstance(b, int)
+    assert b > 1 # greater one or the variance is zero after the first estimate
+    assert isinstance(k, int)
+    assert k > 0
+    assert isinstance(eps, numbers.Real)
+    assert eps > 0
+    assert eps < 1
+    assert SS.isspmatrix(P)
+    assert P.shape[0] >= P.shape[1]
+    assert P.shape[1] == node.n
+
+
+    if not node.is_leaf_node():
+        left = node.left_child
+        right = node.right_child
+
+        n = node.n
+        n1 = left.n
+        n2 = right.n
+        assert n1 + n2 == n
+
+        new_left = estimate_trace(f, b, k, eps, P[:,:n1], left)
+        new_right = estimate_trace(f, b, k, eps, P[:,n1:], right)
+
+        trace_mean = new_left.trace_mean + new_right.trace_mean
+        trace_std = LA.norm([new_left.trace_std, new_right.trace_std])
+
+        new_node = binary_tree.make_internal_node(new_left, new_right, n)
+        new_node.trace_mean = trace_mean
+        new_node.trace_std = trace_std
+
+        return new_node
+
+
+    n = node.n
+    mean = 0.0
+    std = 1.0
+    xs = NP.empty(0)
+
+    random = numpy.random.RandomState(seed=1)
+
+    while k*std > eps * mean:
+        V = 2 * random.randint(0, 2, [n,b]).astype(P.dtype) - 1
+        W = P.H * f(P*V)
+        xs = NP.append( xs, NP.einsum('ij,ij->j', V, W, casting='no') )
+
+        mean = NP.mean(xs)
+        std = NP.std(xs, ddof=1)
+
+
+    new_node = copy.copy(node)
+    new_node.trace_mean = mean
+    new_node.trace_std = std
+
+    return new_node
+
+
+
+def estimate_eigenvalue_count(node, K, M, lambda_1, lambda_c, d, k, eps, b=32):
+    assert isinstance(node, binary_tree.Node)
+    assert SS.isspmatrix(K)
+    assert SS.isspmatrix(M)
+    assert isinstance(lambda_1, numbers.Real)
+    assert lambda_1 > 0
+    assert isinstance(lambda_c, numbers.Real)
+    assert lambda_c > lambda_1
+    assert isinstance(d, int)
+    assert d > 0
+    assert isinstance(k, int)
+    assert k > 0
+    assert isinstance(eps, numbers.Real)
+    assert eps > 0
+    assert eps < 1
+    assert isinstance(b, int)
+    assert b > 1
+
+
+    n = node.n
+    f = polynomial.approximate_projection(d, lambda_1, lambda_c, K, M)
+    P = SS.identity(n, format='csc')
+
+    new_node = estimate_trace(f, b, k, eps, P, node)
+
+    assert isinstance(new_node, binary_tree.Node)
+
+    return new_node
---
+++ python/dcgeig/tests/test_solver.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Copyright 2016 Christoph Conrads
+#
+# This file is part of DCGeig and it is subject to the terms of the DCGeig
+# license. See http://DCGeig.tech/license for a copy of this license.
+
+import unittest
+
+import numpy as NP
+
+import scipy.sparse as SS
+
+import dcgeig.binary_tree as binary_tree
+import dcgeig.solver as solver
+
+
+
+class Test_estimate_trace(unittest.TestCase):
+    def test_simple(self):
+        n = 3
+        m = n-1
+        f = lambda x: x
+        b = 2
+        k = 1
+        eps = 1e-2
+        P = SS.eye(n, m)
+        node = binary_tree.make_leaf_node(m)
+
+        node = solver.estimate_trace(f, b, k, eps, P, node)
+
+        self.assertEqual( node.trace_mean, m )
+
+
+
+class Test_estimate_eigenvalue_count(unittest.TestCase):
+    def test_simple(self):
+        n = 9
+        K = SS.diags(NP.arange(1, n+1), dtype=NP.float64, format='csc')
+        M = SS.identity(n)
+
+        left_child = binary_tree.make_leaf_node(4)
+        right_child = binary_tree.make_leaf_node(5)
+        root = binary_tree.make_internal_node(left_child, right_child, n)
+        eps = 0.05
+
+        new_root = \
+            solver.estimate_eigenvalue_count(root, K, M, 0.75, 1.5, 50, 1, eps)
+
+        mu = new_root.trace_mean
+        std = new_root.trace_std
+
+        self.assertTrue( abs(mu - 1) < eps )
+        self.assertTrue( std < eps )
+
+
+
+if __name__ == '__main__':
+    unittest.main()
