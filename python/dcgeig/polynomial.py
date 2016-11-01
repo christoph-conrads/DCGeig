8751a4c0857996b78e03930522d602732c74f583
Christoph Conrads <gitlab@christoph-conrads.name>
2016-11-01 16:51:21 +0100

    Src: implement, test polynomial code

---
+++ python/dcgeig/polynomial.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Copyright 2016 Christoph Conrads
+#
+# This file is part of DCGeig and it is subject to the terms of the DCGeig
+# license. See http://DCGeig.tech/license for a copy of this license.
+
+import numpy as NP
+import numpy.random
+import numpy.matlib as ML
+import numpy.polynomial.chebyshev as NPC
+
+import scipy.sparse as SS
+import scipy.sparse.linalg as LA
+
+import numbers
+
+
+
+def compute_jackson_coefficients(d):
+    assert isinstance(d, int)
+    assert d >= 0
+
+    if d == 0:
+        return NP.full(1, 1.0)
+
+    k = 1.0 * NP.arange(0, d+1)
+    r = NP.pi / (d+1)
+    cs = (d+1-k) * NP.cos(k*r) + NP.sin(k*r) / NP.tan(r)
+    cs[-1] = 0
+
+    assert cs.size == d+1
+
+    return cs / (d+1)
+
+
+
+def compute_chebyshev_heaviside_coefficients(d):
+    assert isinstance(d, int)
+    assert d >= 0
+
+    k = 1.0 * NP.arange(1, d+1)
+    cs = NP.full(d+1, NP.nan)
+    cs[0] = 0.5
+    cs[1::4] = 1.0
+    cs[2::4] = 0.0
+    cs[3::4] = -1.0
+    cs[4::4] = 0.0
+    cs[1:] = cs[1:] * 2/(NP.pi * k)
+
+    return cs
+
+
+
+def compute_chebyshev_step_coefficients(d, a, b):
+    assert isinstance(d, int)
+    assert d >= 0
+    assert isinstance(a, numbers.Real)
+    assert a >= -1
+    assert a < +1
+    assert isinstance(b, numbers.Real)
+    assert b > -1
+    assert b <= +1
+    assert a < b
+
+
+    # check for the heaviside function
+    if a == 0 and b == 1:
+        return compute_chebyshev_heaviside_coefficients(d)
+
+
+    u = NP.arccos(a)
+    v = NP.arccos(b)
+
+    cs = NP.full(d+1, NP.nan)
+    k = 1.0 * NP.arange(1, d+1)
+
+    cs[0] = u - v
+    cs[1:] = 2 * (NP.sin(k*u) - NP.sin(k*v)) / k
+
+    return cs / NP.pi
+
+
+
+# this function implements the Clenshaw algorithm for monomials(Horner's method)
+def evaluate_matrix_polynomial(ps, c1, c2, solve, K, M, U):
+    assert isinstance(ps, NP.ndarray)
+    assert isinstance(c1, numbers.Real)
+    assert NP.isfinite(c1)
+    assert isinstance(c2, numbers.Real)
+    assert NP.isfinite(c2)
+    assert callable(solve)
+    assert SS.isspmatrix(K)
+    assert SS.isspmatrix(M)
+    assert isinstance(U, NP.ndarray)
+
+    d = len(ps)-1
+    V = ps[d] * U
+    C = c1*M - c2*K
+
+    for i in range(d-1, -1, -1):
+        V = ps[i] * U + solve( C * V )
+
+    return V
+
+
+
+def approximate_projection(d, lambda_1, lambda_c, K, M):
+    assert isinstance(d, int)
+    assert d > 0
+    assert isinstance(lambda_1, numbers.Real)
+    assert lambda_1 > 0
+    assert isinstance(lambda_c, numbers.Real)
+    assert lambda_c > lambda_1
+    assert SS.isspmatrix(K)
+    assert SS.isspmatrix(M)
+    assert NP.all( K.shape == M.shape )
+
+
+    if 2*lambda_1 != lambda_c:
+        raise NotImplementedError('2*lambda_1 != lambda_c')
+
+
+    n = K.shape[0]
+    LU = LA.splu(K, diag_pivot_thresh=0)
+
+    cs = compute_chebyshev_heaviside_coefficients(d)
+    js = compute_jackson_coefficients(d)
+    ps = NPC.cheb2poly(cs * js)
+
+    c1 = 2*lambda_1
+    c2 = 1.0
+
+    def f(V):
+        return evaluate_matrix_polynomial(ps, c1, c2, LU.solve, K, M, V)
+
+    return f
---
+++ python/dcgeig/tests/test_polynomial.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Copyright 2016 Christoph Conrads
+#
+# This file is part of DCGeig and it is subject to the terms of the DCGeig
+# license. See http://DCGeig.tech/license for a copy of this license.
+
+import unittest
+
+import numpy as NP
+import numpy.linalg as LA
+
+import scipy.sparse as SS
+
+import dcgeig.polynomial as polynomial
+
+
+
+class Test_compute_jackson_coefficients(unittest.TestCase):
+    def test_degree_0(self):
+        js = polynomial.compute_jackson_coefficients(0)
+
+        self.assertIsInstance( js, NP.ndarray )
+        self.assertEqual( js.size, 1 )
+        self.assertEqual( js[0], 1 )
+
+
+    def test_degrees(self):
+        for d in range(1,10):
+            js = polynomial.compute_jackson_coefficients(d)
+
+            self.assertIsInstance( js, NP.ndarray )
+
+            self.assertEqual( js.size, d+1 )
+            self.assertEqual( js[0], 1 )
+            self.assertEqual( js[-1], 0 )
+
+            self.assertTrue( NP.all(js[:-1] > 0) )
+            self.assertTrue( NP.all(js[1:] < 1) )
+
+
+
+class Test_compute_chebyshev_heaviside_coefficients(unittest.TestCase):
+    def test_simple(self):
+        for d in range(0, 10):
+            cs = polynomial.compute_chebyshev_heaviside_coefficients(d)
+
+            self.assertIsInstance( cs, NP.ndarray )
+            self.assertEqual( cs.size, d+1 )
+            self.assertEqual( cs[0], 0.5 )
+            self.assertTrue( LA.norm(cs) < 1 )
+
+
+
+class Test_compute_chebyshev_step_coefficients(unittest.TestCase):
+    def test_simple(self):
+        for d in range(0, 10):
+            for a in NP.arange(-1, +1, 0.1):
+                for b in NP.arange(a+0.1, +1, 0.1):
+                    cs = polynomial.compute_chebyshev_step_coefficients(d, a, b)
+
+                    self.assertIsInstance( cs, NP.ndarray )
+                    self.assertEqual( cs.size, d+1 )
+                    self.assertTrue( min(cs) > -1 )
+                    self.assertTrue( max(cs) < +1 )
+
+
+
+class Test_evaluate_matrix_polynomial(unittest.TestCase):
+    def test_simple(self):
+        for n in range(1, 5):
+            ps = NP.full(1, 1.0)
+            solve = lambda x: x
+            K = SS.identity(n)
+            M = SS.identity(n)
+
+            u = NP.ones([n,1])
+            v = polynomial.evaluate_matrix_polynomial(ps, 1, 0, solve, K, M, u)
+
+            self.assertEqual( LA.norm(u-v), 0 )
+
+
+    def test_max_exponent(self):
+        n = 4
+
+        for d in range(1, 5):
+            ps = NP.full(d+1, 0.0)
+            ps[-1] = 1
+            solve = lambda x: x
+            K = SS.identity(n)
+            M = SS.identity(n)
+
+            u = NP.reshape(NP.arange(1, n+1), [n,1])
+            v = polynomial.evaluate_matrix_polynomial(ps, 2, 0, solve, K, M, u)
+
+            self.assertTrue( LA.norm(2**d * v - u), 0 )
+
+
+    def test_all_ones(self):
+        n = 4
+        m = 3
+
+        for d in range(1, 5):
+            ps = NP.ones(d+1)
+            solve = lambda x: x
+            K = SS.identity(n)
+            M = SS.identity(n)
+
+            U = 1.0 * NP.reshape(NP.arange(1, m*n+1), [n,m])
+            V = polynomial.evaluate_matrix_polynomial(ps, 1, 0, solve, K, M, U)
+
+            self.assertEqual( LA.norm(V - (d+1)*U), 0 )
+
+
+
+class Test_approximate_projection(unittest.TestCase):
+    def test_simple(self):
+        n = 4
+        m = 3
+
+        l = 0.75
+        r = 1.5
+
+        K = SS.diags( 1.0 * NP.arange(1,n+1), format='csc' )
+        M = SS.identity(n)
+
+        for d in [50,72,73,100]:
+            f = polynomial.approximate_projection(d, l, r, K, M)
+
+            U = NP.identity(n)
+            V = f(U)
+            
+            eps = NP.finfo(NP.float32).eps
+            self.assertTrue( abs(NP.trace(V) - 1) <= eps )
+
+
+
+if __name__ == '__main__':
+    unittest.main()
